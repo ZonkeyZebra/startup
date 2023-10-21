e4f9ebaa1f53bee873be81be87b3fa531cb7c006
ZonkeyZebra <blackcec@yahoo.com>
2023-10-21 12:35:30 -0700

    added user-reviews js

---
+++ user-reviews.js
@@ -0,0 +1,41 @@
+function loadReviews() {
+    let review = [];
+    const scoresText = localStorage.getItem('review');
+    if (scoresText) {
+      scores = JSON.parse(scoresText);
+    }
+  
+    const reviewBoxEl = document.querySelector('#review');
+  
+    if (review.length) {
+      for (const [i, review] of review.entries()) {
+        const restaurantEl = document.createElement('p');
+        const averageRateEl = document.createElement('p');
+        const userRateEl = document.createElement('p');
+        const reviewTextEl = document.createElement('p');
+        const locationEl = document.createElement('p');
+        const reviewDateEl = document.createElement('p');
+  
+        restaurantEl.textContent = review.restaurant;
+        averageRateEl.textContent = review.average;
+        userRateEl.textContent = review.rate;
+        reviewTextEl.textContent = review.text;
+        locationEl.textContent = review.location;
+        reviewDateEl.textContent = review.date;
+  
+        const reviewBox = document.createElement('div');
+        reviewBox.appendChild(restaurantEl);
+        reviewBox.appendChild(averageRateEl);
+        reviewBox.appendChild(userRateEl);
+        reviewBox.appendChild(reviewTextEl);
+        reviewBox.appendChild(locationEl);
+        reviewBox.appendChild(reviewDateEl);
+  
+        reviewBoxEl.appendChild(reviewBox);
+      }
+    } else {
+        reviewBoxEl.innerHTML = 'You have not left any reviews yet!';
+    }
+  }
+  
+  loadReviews();

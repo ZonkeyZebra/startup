d9c6c4c7600b4a0b8264a4315b52e9b703aaab2d
ZonkeyZebra <blackcec@yahoo.com>
2023-10-23 20:12:14 -0700

    updated login to take you to review.html

--- login.js
+++ login.js
@@ -1,5 +1,5 @@
 function login() {
     const nameEl = document.querySelector("#name");
     localStorage.setItem("userName", nameEl.value);
-    window.location.href = "search.html";
+    window.location.href = "review.html";
   }

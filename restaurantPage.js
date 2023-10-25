3b7f1408580c873312f66936cc777e50b374552d
ZonkeyZebra <blackcec@yahoo.com>
2023-10-24 18:30:01 -0700

    added restaurantPage.js

---
+++ restaurantPage.js
@@ -0,0 +1,19 @@
+function loadRestaurantReviews() {
+    console.log("hello?");
+  
+    console.log(localStorage.getItem('review'));
+  
+    const stringReview = localStorage.getItem('review');
+  
+    let theRestaurantReviews = [];
+    theRestaurantReviews = stringReview.split(",");
+    console.log(theRestaurantReviews);
+  
+    const theirReviews = document.querySelector('.restaurantReviews');
+    theirReviews.innerHTML = `<div class="review">
+    <p class="user-rate">Rating: <span class="result">${theRestaurantReviews[2]}</span></p>
+    <p class="review-text">${theRestaurantReviews[3]}</p>
+    <p class="location">Location of review: <span class="result">${theRestaurantReviews[4]}</span></p>
+    <p class="date">${theRestaurantReviews[5]}</p>
+    </div>`;
+  }

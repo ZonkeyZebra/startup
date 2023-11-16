3d91abc1a87b0070f4452838280889407097d995
ZonkeyZebra <blackcec@yahoo.com>
2023-11-15 20:13:04 -0800

    working basic login

--- database.js
+++ database.js
@@ -1,10 +1,13 @@
 const { MongoClient } = require('mongodb');
 const config = require('./dbConfig.json');
+const bcrypt = require('bcrypt');
+const uuid = require('uuid');
 
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
 const client = new MongoClient(url);
 const db = client.db('pickyourplate');
 const reviewCollection = db.collection('review');
+const userCollection = db.collection('user');
 
 // This will asynchronously test the connection and exit the process if it fails
 (async function testConnection() {
@@ -27,4 +30,26 @@ function getReviews() {
     return cursor.toArray();
 }
 
-module.exports = { addReview, getReviews };
+function getUser(username) {
+  return userCollection.findOne({ username: username });
+}
+
+function getUserByToken(token) {
+  return userCollection.findOne({ token: token });
+}
+
+async function createUser(username, password) {
+  // Hash the password before we insert it into the database
+  const passwordHash = await bcrypt.hash(password, 10);
+
+  const user = {
+    username: username,
+    password: passwordHash,
+    token: uuid.v4(),
+  };
+  await userCollection.insertOne(user);
+
+  return user;
+}
+
+module.exports = { addReview, getReviews, getUser, getUserByToken, createUser };
--- public/login.js
+++ public/login.js
@@ -5,3 +5,75 @@ function login() {
     //redirects them to review page when they login
     window.location.href = "review.html";
   }
+
+  (async () => {
+    const userName = localStorage.getItem('userName');
+    if (userName) {
+      document.querySelector('#playerName').textContent = userName;
+      setDisplay('loginControls', 'none');
+      setDisplay('playControls', 'block');
+    } else {
+      setDisplay('loginControls', 'block');
+      setDisplay('playControls', 'none');
+    }
+  })();
+  
+  async function loginUser() {
+    loginOrCreate(`/api/auth/login`);
+  }
+  
+  async function createUser() {
+    loginOrCreate(`/api/auth/create`);
+  }
+  
+  async function loginOrCreate(endpoint) {
+    const userName = document.querySelector('#userName')?.value;
+    const password = document.querySelector('#userPassword')?.value;
+    const response = await fetch(endpoint, {
+      method: 'post',
+      body: JSON.stringify({ username: userName, password: password }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    });
+  
+    if (response.ok) {
+      localStorage.setItem('userName', userName);
+      window.location.href = 'review.html';
+    } else {
+      const body = await response.json();
+      const modalEl = document.querySelector('#msgModal');
+      modalEl.querySelector('.modal-body').textContent = `⚠ Error: ${body.msg}`;
+      const msgModal = new bootstrap.Modal(modalEl, {});
+      msgModal.show();
+    }
+  }
+  
+  function startReviewing() {
+    window.location.href = 'review.html';
+  }
+  
+  function logout() {
+    localStorage.removeItem('userName');
+    fetch(`/api/auth/logout`, {
+      method: 'delete',
+    }).then(() => (window.location.href = '/'));
+  }
+  
+  async function getUser(email) {
+    let reviews = {};
+    // See if we have a user with the given email.
+    const response = await fetch(`/api/user/${email}`);
+    if (response.status === 200) {
+      return response.json();
+    }
+  
+    return null;
+  }
+  
+  function setDisplay(controlId, display) {
+    const playControlEl = document.querySelector(`#${controlId}`);
+    if (playControlEl) {
+      playControlEl.style.display = display;
+    }
+  }

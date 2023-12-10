799bca030f90c331ed7be501f0394eeebe8e8e1b
ZonkeyZebra <blackcec@yahoo.com>
2023-12-09 16:26:04 -0800

    starting to try and get websocket to work

--- src/review/review.jsx
+++ src/review/review.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { ReviewEvent, ReviewNotifier } from "./liveUpdates";
 
 export function Review(props) {
     const [userName, setUserName] = React.useState(props.userName);
@@ -22,7 +23,7 @@ export function Review(props) {
 
             const reviews = await response.json();
             localStorage.setItem('reviews', JSON.stringify(reviews));
-
+            ReviewNotifier.broadcastEvent(userName, ReviewEvent.Review, 'left a review!');
         }catch {
                 // If there was an error then just track locally
                 const updatedReviews = [...allReviews, newReview];
@@ -82,64 +83,8 @@ export function Review(props) {
 }
 
 //*******************************************************OLD JS BELOW ********************************************************
-//   //gets a users username they logged in with
-//   function getUserName() {
-//     return localStorage.getItem('userName');
-//   }
 
-//   let allReviews = [];
 //   const ReviewEvent = 'reviewLeft';
-//   const userName = this.getUserName();
-
-//   //gets the values inputted from the form and saves them to local storage
-//   async function saveReview() {
-    
-//     const restaurantEl = document.querySelector("#selectRestaurant");
-//     localStorage.setItem("restaurant", restaurantEl.value);
-//     console.log(restaurantEl);
-
-//     const ratingEl = document.querySelector("#selectRating");
-//     localStorage.setItem("rating", ratingEl.value);
-
-//     const commentEl = document.querySelector("#leaveComment");
-//     localStorage.setItem("comment", commentEl.value);
-
-//     const locationEl = document.querySelector("#leaveLocation");
-//     localStorage.setItem("location", locationEl.value);
-
-//     const date = new Date().toLocaleDateString();
-    
-//     const review = [userName, localStorage.getItem("restaurant"), localStorage.getItem("rating"), localStorage.getItem("comment"), localStorage.getItem("location"), date];
-//     const newReview = {name: userName, restaurant: restaurantEl.value, rating: ratingEl.value, comment: commentEl.value, location: locationEl.value, date: date}
-//     console.log(review);
-//     console.log("object review");
-//     console.log(newReview)
-//     try {
-//       const response = await fetch('/api/review', {
-//         method: 'POST',
-//         headers: {'content-type': 'application/json'},
-//         //body: JSON.stringify(review),
-//         body: JSON.stringify(newReview),
-//     });
-//     console.log("response below");
-//     console.log(response);
-    
-//     // Store what the service gave us
-//     const reviews = await response.json();
-//     console.log(reviews);
-//     console.log(JSON.stringify(reviews));
-//     localStorage.setItem('reviews', JSON.stringify(reviews));
-
-//     // Let other user know a review has been left
-//     this.broadcastEvent(userName, ReviewEvent);
-//   } catch {
-//     // If there was an error then just track locally
-//     allReviews = allReviews.concat(review);
-//     localStorage.setItem("allReviews", allReviews);
-//   }
-//   //clears out the form after it's been submitted
-//   document.getElementById("reviewForm").reset();
-//   }
 
 //   //actual WebSocket live updates
 //   function configureWebSocket() {
